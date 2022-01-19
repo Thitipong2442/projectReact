d6b0efaba83f45f68f6ba9c8f86a63c947c30ac9
Thitipong2442 <71888801+Thitipong2442@users.noreply.github.com>
2022-01-19 12:49:01 +0700

    projectReact-Navigation
    projectReact-Navigation

--- 19-1-2022/screens/HomeScreen.js
+++ 19-1-2022/screens/HomeScreen.js
@@ -3,7 +3,44 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import AboutScreen from "./AboutScreen";
 import { Ionicons } from "@expo/vector-icons";
 
+import {
+    HeaderButtons,
+    HeaderButton,
+    Item,
+    HiddenItem,
+    OverflowMenu,
+} from "react-navigation-header-buttons";
+
+const IoniconsHeaderButton = (props) => (
+    // the props here come from <Item ... />
+    // you may access them and pass something else to HeaderButton if you like
+    <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
+
 const HomeScreen = ({ navigation }) => {
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+                    <Item
+                        title="menu"
+                        iconName="menu"
+                        onPress={() => alert("เมนูการทำงาน")}
+                    />
+                </HeaderButtons>
+            ),
+            headerRight: () => (
+                <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+                    <Item
+                        title="register"
+                        iconName="person-add"
+                        onPress={() => alert("ลงทะเบียน")}
+                    />
+                </HeaderButtons>
+            ),
+        });
+    }, [navigation]);
+
     return (
         <View style={styles.container}>
             <Ionicons name="ios-home" size={30} color="#1974D2" />
